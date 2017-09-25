dcfa71966c849424f4dec29d38d6308d3f05f610
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2017-09-25 14:14:37 +0300

    Added template for temperature data

---
+++ data.py
@@ -0,0 +1,20 @@
+"""data.py
+
+Description:
+    
+    This file contains FMI temperature data from Helsinki Malmi Airport that are used 
+    in Exercise 4 at Geo-Python 2017 course: https://geo-python.github.io
+
+Note:
+    
+    The data values in temp list are hourly values in represented Fahrenheits. 
+   
+
+
+Henrikki Tenkanen - 25.9.2017
+
+"""
+
+# List of hourly temperature values (in Fahrenheits)
+temp_data = []
+

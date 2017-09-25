c465ccaff6dd9d69b9831f72a3abd5ef0eae7225
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2017-09-25 14:53:26 +0300

    Added data for one week

--- data.py
+++ data.py
@@ -2,19 +2,33 @@
 
 Description:
     
-    This file contains FMI temperature data from Helsinki Malmi Airport that are used 
+    This file contains FMI temperature data from Helsinki Malmi Airport for one week that are used 
     in Exercise 4 at Geo-Python 2017 course: https://geo-python.github.io
+    
+Data source:
+    https://www.ncdc.noaa.gov
 
 Note:
-    
-    The data values in temp list are hourly values in represented Fahrenheits. 
+    The data values in temp list are half-hourly values (1 obs / 30 minutes) represented in Fahrenheits. 
    
-
-
-Henrikki Tenkanen - 25.9.2017
+Author:
+    Henrikki Tenkanen - 25.9.2017
 
 """
 
-# List of hourly temperature values (in Fahrenheits)
-temp_data = []
+# List of half-hourly temperature values (in Fahrenheits) for one week
+temp_data = [19, 21, 21, 21, 23, 23, 23, 21, 19, 21, 19, 21, 23, 27, 27, 28, 30, 30, 32, 32, 32, 32, 34, 34,
+             34, 36, 36, 36, 36, 36, 36, 34, 34, 34, 34, 34, 34, 32, 30, 30, 30, 28, 28, 27, 27, 27, 23, 23,
+             21, 21, 21, 19, 19, 19, 18, 18, 21, 27, 28, 30, 32, 34, 36, 37, 37, 37, 39, 39, 39, 39, 39, 39,
+             41, 41, 41, 41, 41, 39, 39, 37, 37, 36, 36, 34, 34, 32, 30, 30, 28, 27, 27, 25, 23, 23, 21, 21,
+             19, 19, 19, 18, 18, 18, 21, 25, 27, 28, 34, 34, 41, 37, 37, 39, 39, 39, 39, 41, 41, 39, 39, 39,
+             39, 39, 41, 39, 39, 39, 37, 36, 34, 32, 28, 28, 27, 25, 25, 25, 23, 23, 23, 23, 21, 21, 21, 21,
+             19, 21, 19, 21, 21, 19, 21, 27, 28, 32, 36, 36, 37, 39, 39, 39, 39, 39, 41, 41, 41, 41, 41, 41,
+             41, 41, 41, 39, 37, 36, 36, 34, 32, 30, 28, 28, 27, 27, 25, 25, 23, 23, 23, 21, 21, 21, 19, 19,
+             19, 19, 19, 19, 21, 23, 23, 23, 25, 27, 30, 36, 37, 37, 39, 39, 41, 41, 41, 39, 39, 41, 43, 43,
+             43, 43, 43, 43, 43, 43, 43, 39, 37, 37, 37, 36, 36, 36, 36, 34, 32, 32, 32, 32, 30, 30, 28, 28,
+             28, 27, 27, 27, 27, 25, 27, 27, 27, 28, 28, 28, 30, 32, 32, 32, 34, 34, 36, 36, 36, 37, 37, 37,
+             37, 37, 37, 37, 37, 37, 36, 34, 30, 30, 27, 27, 25, 25, 23, 21, 21, 21, 21, 19, 19, 19, 19, 19,
+             18, 18, 18, 18, 18, 19, 23, 27, 30, 32, 32, 32, 32, 32, 32, 34, 34, 34, 34, 34, 36, 36, 36, 36,
+             36, 32, 32, 32, 32, 32, 32, 32, 32, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 28, 28]
 
